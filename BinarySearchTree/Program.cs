a271063dd8fd2992715483f34a6d4d676a7934fc
mithu-mawhinney <mithumk4@gmail.com>
2024-03-27 12:03:29 +0000

    Binary search tree

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bst myBinarySearchTree = new Bst(13);
+            Console.WriteLine($"The root of the Binary search tree is {myBinarySearchTree.getRoot().value}");
+        }
+    }
+}
+
